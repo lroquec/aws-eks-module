1d63b651165eb8bff9f3d25cdcc6e0ac9cc03db4
Laura Roque <lroquecu@gmail.com>
2025-02-17 09:29:27 +0100

    refactor(terraform) Remove unused availability zones data block from main configuration

--- 1-main.tf
+++ 1-main.tf
@@ -1,9 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
 locals {
   admin_role_name     = "${var.name_prefix}-admin"
   developer_role_name = "${var.name_prefix}-developer"
