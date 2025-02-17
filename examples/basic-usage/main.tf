5250cfa0c7595b0efcef7a8dbee40eb1d0980fe6
Laura Roque <lroquecu@gmail.com>
2025-02-17 09:32:57 +0100

    fix(terraform) Specify required providers for AWS and null in basic usage example

--- examples/basic-usage/main.tf
+++ examples/basic-usage/main.tf
@@ -4,6 +4,17 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
 }
 
 module "eks" {
