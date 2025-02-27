5438aade576a2f1e7f1751d3f938de1a312ba76c
Laura Roque <lroquecu@gmail.com>
2025-02-27 08:54:54 +0100

    chore(terraform): Add default values for Grafana admin password and ingress host variables

--- examples/basic-usage/variables.tf
+++ examples/basic-usage/variables.tf
@@ -1,9 +1,11 @@
 variable "grafana_admin_password" {
   description = "Admin password for Grafana"
   type        = string
+  default = "prom-operator"
 }
 
 variable "grafana_ingress_host" {
   description = "Grafana Ingress Host"
   type        = string
+  default     = "grafana.example.com"
 }
